b071a3f3aa4ff805caf68dfd4995a1e6de86b50e
Mashqur Habib <mashqur.himel@gmail.com>
2016-02-05 02:47:08 +0600

    example array

---
+++ Array/Array.java
@@ -0,0 +1,11 @@
+class Array{
+
+	public static void main(String[] args){
+		String[][] array = {{"Hi","Bye","Welcome"},
+			{"Maheraj.","Ruhul."},{"How are you?"}
+		};
+		System.out.println(array[0][0] +" "+ array[1][0] +" "+ array[2][0]);
+		System.out.println(array[0][1] +" "+array[1][1]);
+		System.out.println(array.length);
+	}
+}
