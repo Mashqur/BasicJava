97c70423d5f5037c7dbfeabd96c1a84540eba141
himel <mashqur.himel@gmail.com>
2016-02-09 19:42:45 +0600

    while and do-while loop example

---
+++ do-while/dowhiledemo.java
@@ -0,0 +1,9 @@
+class dowhiledemo{
+	public static void main(String[] args){
+		int count = 1;
+		do{
+			System.out.println(count);
+			count++;
+		}while(count<11);
+	}
+}
---
+++ while/whileDemo.java
@@ -0,0 +1,9 @@
+class whileDemo{
+	public static void main(String[] args){
+		int count = 1;
+		while(count<10){
+			System.out.println(count);
+			count++;
+		}
+	}
+}
